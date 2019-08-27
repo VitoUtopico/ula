ee475efc8c342576bbc9aa3a5a03b1f46158adb4
Fábio Vito <fabiovito@hotmail.com>
2019-08-27 09:03:24 -0300

    Add funcoes de soma e subtracao e operadores and e or

--- main_ula.py
+++ main_ula.py
@@ -1,4 +1,6 @@
-
+reg_a = 0
+reg_b = 0
+op = 0
 
 def menu_inicial():
     return int(input('Seja muito bem vindo à ULA.PY\n\n\
@@ -14,37 +16,68 @@ def menu_inicial():
           Escolha a opção => '))
 
 def def_reg_a():
-    print( 'registrador a')
+    global reg_a
+    reg_a = int(input('Insira o valor do registrador A: '))
 
 def def_reg_b():
-    pass
+    global reg_b
+    reg_b = int(input('Insira o valor do registrador B: '))
 
 def le_reg_a():
-    pass
+    global reg_a
+    print(reg_a)
 
 def le_reg_b():
-    pass
+    global reg_b
+    print(reg_b)
 
 def le_reg_flag():
     pass
 
 def def_op():
-    pass
-
+    global op
+    op = input('\
+               00 -> Soma\n\
+               01 -> Subtração\n\
+               10 -> AND\n\
+               11 -> OR\n\
+               \nEscolha o operador => ')
+   
 def exec_ula():
-    pass
+    global op, reg_a, reg_b
+    if op == '00':
+        reg_a += reg_b
+        return
+    elif op == '01':
+        reg_a -= reg_b
+        return
+    elif op == '10':
+        reg_a = reg_a & reg_b
+        return
+    elif op == '11':
+        reg_a = reg_a | reg_b
+        return
 
 
 
 def processa_menu_inicial(entrada):
-    if entrada == 1:  return def_reg_a()
-    if entrada == 2:  return def_reg_b()
-    if entrada == 3:  return le_reg_a()
-    if entrada == 4:  return le_reg_b()
-    if entrada == 5:  return le_reg_flag()
-    if entrada == 6:  return def_op()
-    if entrada == 7:  return exec_ula()
-    if entrada == 8:  return 
+    if entrada == 1:  
+        def_reg_a()
+    elif entrada == 2:
+        def_reg_b()
+    elif entrada == 3:
+        le_reg_a()
+    elif entrada == 4:
+        le_reg_b()
+    elif entrada == 5:
+        le_reg_flag()
+    elif entrada == 6:
+        def_op()
+    elif entrada == 7:
+        exec_ula()
+    elif entrada == 8:
+        return 
+    processa_menu_inicial(menu_inicial())
         
     
 processa_menu_inicial(menu_inicial())
